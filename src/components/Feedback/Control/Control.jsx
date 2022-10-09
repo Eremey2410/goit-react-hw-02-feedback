e5827d6f2c9eed9de0eb42e7f034a85f30944df4
Eremey2410 <2410esv@gmail.com>
2022-10-09 11:01:06 +0300

    add propTypes

--- src/components/Feedback/Control/Control.jsx
+++ src/components/Feedback/Control/Control.jsx
@@ -1,10 +1,11 @@
+import PropTypes from 'prop-types';
+
 import { FeedbackList, Buttons, FeedbackTitle } from './Control.styled';
 
 export const FeedbackOptions = ({ onLeaveFeedback, options }) => {
   const handleBtnClick = evt => {
     onLeaveFeedback(evt.target.textContent);
   };
-
   return (
     <section>
       <FeedbackTitle>Please leave feedback</FeedbackTitle>
@@ -20,4 +21,8 @@ export const FeedbackOptions = ({ onLeaveFeedback, options }) => {
       </FeedbackList>
     </section>
   );
+};
+FeedbackOptions.propTypes = {
+  onLeaveFeedback: PropTypes.func.isRequired,
+  options: PropTypes.arrayOf(PropTypes.string),
 };
